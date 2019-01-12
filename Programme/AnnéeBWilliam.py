0cb81c108d6b7078bda553cf0c70988ef3da95a3
Willilli <46615386+Willilli@users.noreply.github.com>
2019-01-12 03:32:16 +0100

    Add files via upload

---
+++ Programme/AnnéeBWilliam.py
@@ -0,0 +1,15 @@
+﻿# Programme testant si une année, saisie par l'utilisateur,
+# est bissextile ou non
+
+annee=input("Saisis année:")
+
+annee=int(annee)
+if annee%4 !=0:
+   print("Année non bissextile")
+elif annee%4 == 0 and annee%100==0 :
+ if annee%400 == 0:
+    print("Année bissextile")
+ elif annee%400 !=0:
+    print("Année non bissextile")
+elif annee%4 == 0 and annee%100!=0 :
+   print("Année bissextile")
